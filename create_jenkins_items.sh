d114c14c6c4ffaa9faac8cd1f8b98442e5ce3548
root <root@centos06_devops.(none)>
2017-06-18 17:34:19 -0600

    Add shell script to load jenkins jobs and views, etc.

---
+++ create_jenkins_items.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+	JENKINS_CLI_CMD='java -jar /usr/local/share/applications/jenkins-cli.jar -s http://localhost:8080 -auth admin:`cat ~jenkins/secrets/initialAdminPassword`'
+	for CONFIG_INPUT in *.xml
+	do
+		ITEM_TYPE=`basename ${CONFIG_INPUT} | cut -d'-' -f1`
+		ITEM_NAME=`basename ${CONFIG_INPUT} | cut -d'-' -f2- \
+			| cut -d'.' -f1`
+		echo "Loading ${ITEM_TYPE}: ${ITEM_NAME}..."
+		${JENKINS_CLI_CMD} create-${ITEM_TYPE} ${ITEM_NAME} < ${CONFIG_INPUT}
+	done
+	exit
